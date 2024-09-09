cb351c9a98084b9e2f91ed462ff9c679e3aee529
bhaveshchadha <bhaveshchadha14@gmail.com>
2024-09-09 18:32:14 +0530

    add: + sign added to each answer

--- src/components/Accordions.jsx
+++ src/components/Accordions.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-
+import { FaPlus } from "react-icons/fa6";
 function Accordions() {
   return (
     <div className="container">
-      <div className="box">Enable Multi Sections</div>
-      <div className="box">Lorem ipsum dolor sit amet.</div>
+      <div className="box question">Enable Multi Sections</div>
       <div className="box">
-        Lorem, ipsum dolor sit amet consectetur adipisicing.
+        Lorem ipsum dolor sit amet.
+        <FaPlus />
       </div>
-      <div className="box">Lorem ipsum dolor sit amet consectetur.</div>
-      <div className="box">Lorem ipsum dolor sit amet consectetur.</div>
+      <div className="box">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
+        possimus provident. Quidem!
+        <FaPlus />
+      </div>
+      <div className="box">
+        Lorem ipsum dolor sit amet consectetur.
+        <FaPlus />
+      </div>
+      <div className="box">
+        Lorem ipsum dolor sit amet consectetur.
+        <FaPlus />
+      </div>
     </div>
   );
 }
