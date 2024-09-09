53650116a8ddc6910075c9bd13b74962bd8f2b25
bhaveshchadha <bhaveshchadha14@gmail.com>
2024-09-09 18:52:33 +0530

    fix: fixed classnames

--- src/components/Accordions.jsx
+++ src/components/Accordions.jsx
@@ -3,23 +3,31 @@ import { FaPlus } from "react-icons/fa6";
 function Accordions() {
   return (
     <div className="container">
-      <div className="box  question">Enable Multi Sections</div>
-      <div className="box answer">
-        Lorem ipsum dolor sit amet.
-        <FaPlus size={10} />
+      <div className="box  title">Enable Multi Sections</div>
+      <div className="box">
+        <div className=" question">
+          Lorem ipsum dolor sit amet.
+          <FaPlus className=" show_answer" size={10} />
+        </div>
       </div>
-      <div className="box answer">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
-        possimus provident. Quidem!
-        <FaPlus size={10} />
+      <div className="box">
+        <div className="question">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
+          possimus provident. Quidem!
+          <FaPlus className=" show_answer" size={10} />
+        </div>
       </div>
-      <div className="box answer">
-        Lorem ipsum dolor sit amet consectetur.
-        <FaPlus size={10} />
+      <div className="box">
+        <div className="question">
+          Lorem ipsum dolor sit amet consectetur.
+          <FaPlus className=" show_answer" size={10} />
+        </div>
       </div>
-      <div className="box answer">
-        Lorem ipsum dolor sit amet consectetur.
-        <FaPlus size={10} />
+      <div className="box">
+        <div className="question">
+          Lorem ipsum dolor sit amet consectetur.
+          <FaPlus className=" show_answer" size={10} />
+        </div>
       </div>
     </div>
   );
