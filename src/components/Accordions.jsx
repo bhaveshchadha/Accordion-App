86e3a89fd31ba168950823e09d4fb58f85911f20
bhaveshchadha <bhaveshchadha14@gmail.com>
2024-09-13 16:31:27 +0530

    add: added tailwind css and daisy ui

--- src/components/Accordions.jsx
+++ src/components/Accordions.jsx
@@ -4,7 +4,35 @@ import AccordionItem from "./AccordionItem";
 
 function Accordions() {
   return (
-    <></>
+    <>
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" defaultChecked />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+    </>
     // <div className="container">
     //   <div className="box title"
     //   </div>
