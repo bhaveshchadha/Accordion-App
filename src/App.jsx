d3926c22cc8eab1b68b1965a48cfc4d2c5dc833d
bhaveshchadha <bhaveshchadha14@gmail.com>
2024-09-14 17:32:04 +0530

    In here

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import Accordions from "./components/Accordions";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Accordions />
